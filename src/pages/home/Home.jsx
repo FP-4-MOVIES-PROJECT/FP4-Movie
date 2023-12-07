8aecfd612a4f1a15748b7a26041aa1e80c7bcae5
Bella621 <92489927+Bella621@users.noreply.github.com>
2023-12-07 16:18:23 +0700

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+import HeroBanner from './heroBanner/HeroBanner';
+
+HeroBanner;
+
+const Home = () => {
+  return (
+    <div className='homePage'>
+        <HeroBanner/>
+    </div>
+  );
+};
+
+export default Home
