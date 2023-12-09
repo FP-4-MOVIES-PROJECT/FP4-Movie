28f8ae1a0807e43ff5885c55be06cd3504a8fafe
Yurio Dwi LS <rea.riera@gmail.com>
2023-12-09 22:20:05 +0700

    card, details, play video

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { fetchDataFromApi } from './utils/api';
+import { fetchDataFromApi } from "./utils/api";
 
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
@@ -15,57 +16,60 @@ import PageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
-    const { url } = useSelector((state) => state.home);
-    console.log(url);
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
 
-    useEffect(() => {
-      fetchApiConfig();
-      genresCall();
+  useEffect(() => {
+    fetchApiConfig();
+    genresCall();
   }, []);
 
   const fetchApiConfig = () => {
-      fetchDataFromApi("/configuration").then((res) => {
-          console.log(res);
-          const url = {
-            backdrop: res.images.secure_base_url + "original",
-            poster: res.images.secure_base_url + "original",
-            profile: res.images.secure_base_url + "original",
-        };
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
 
-        dispatch(getApiConfiguration(url));
+      dispatch(getApiConfiguration(url));
     });
-};
-const genresCall = async () => {
-  let promises = [];
-  let endPoints = ["tv", "movie"];
-  let allGenres = {};
+  };
 
-  endPoints.forEach((url) => {
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
       promises.push(fetchDataFromApi(`/genre/${url}/list`));
-  });
+    });
 
-  const data = await Promise.all(promises);
-  console.log(data);
-  data.map(({ genres }) => {
+    const data = await Promise.all(promises);
+    console.log(data);
+    data.map(({ genres }) => {
       return genres.map((item) => (allGenres[item.id] = item));
-  });
+    });
 
-  dispatch(getGenres(allGenres));
-};
+    console.log(allGenres);
 
-return (
-  <BrowserRouter>
+    dispatch(getGenres(allGenres));
+  };
+
+  return (
+    <BrowserRouter>
       <Header />
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:mediaType/:id" element={<Details />} />
-          <Route path="/search/:query" element={<SearchResult />} />
-          <Route path="/explore/:mediaType" element={<Explore />} />
-          <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
-  </BrowserRouter>
-);
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,20 @@
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
+
 import "./style.scss";
-
-import HeroBanner from './heroBanner/HeroBanner';
-
-HeroBanner;
 
 const Home = () => {
   return (
-    <div className='homePage'>
-        <HeroBanner/>
+    <div className="homePage">
+      <HeroBanner />
+      <Trending />
+      <Popular />
+      <TopRated />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };
 
-export default Home
+export default Home;
